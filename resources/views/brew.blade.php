44284ec8bcaedba033aa57addecbc3f57550664c
wiiqwooq <6014120021@mutacth.com>
2020-02-17 16:14:47 +0700

    commit all

---
+++ resources/views/brew.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Brew</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wombat Coffee Roasters</title>
+
+    <link rel="stylesheet" href="css/listing2.css">
+    <link rel="stylesheet" href="css/listing3.css">
+    <link rel="stylesheet" href="css/listing5.css">
+    <link rel="stylesheet" href="css/listing7.css">
+    <link rel="stylesheet" href="css/listing8.css">
+    <link rel="stylesheet" href="css/listing9.css">
+    <link rel="stylesheet" href="css/listing10.css">
+  </head>
+  <body>
+    <header id="header" class="page-header">
+        <div class="title">
+          <h1>Wombat Coffee Roasters</h1>
+          <div class="slogan">We love coffee</div>
+        </div>
+</header>
+
+        <nav class="menu" id="main-menu">
+        <button class="menu-toggle" id="toggle-menu">
+          toggle menu
+        </button>
+        <div class="menu-dropdown">
+          <ul class="nav-menu">
+            <li><a href="/">About</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/brew">Brew</a></li>
+          </ul>
+        </div>
+        </nav>
+    <h4>Milk Green Tea</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/0080ba22e8a34700b18a7d9165819add.png" width="300" height="310">
+    <br>
+    <h4>Thai Tea</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/9352e582ef95453f807ea255a5247e73.png" width="300" height="310">
+    <br>
+    <h4>Lemon Tea</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/1e21182bbb9e4cd0900fe8ac5b1e9d29.png" width="300" height="310">
+    <br>
+    <h4>Black Tea</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/28dbd669317e482783ee86767965fe71.png" width="300" height="310">
+    <br>
+    <h4>Black Tea Honey Lemon</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/1b054e7e591349a78bc4df570d80a0a8.png" width="300" height="310">
+    <br>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Menu</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wombat Coffee Roasters</title>
+
+    <link rel="stylesheet" href="css/listing2.css">
+    <link rel="stylesheet" href="css/listing3.css">
+    <link rel="stylesheet" href="css/listing5.css">
+    <link rel="stylesheet" href="css/listing7.css">
+    <link rel="stylesheet" href="css/listing8.css">
+    <link rel="stylesheet" href="css/listing9.css">
+    <link rel="stylesheet" href="css/listing10.css">
+  </head>
+  <body>
+    <header id="header" class="page-header">
+        <div class="title">
+          <h1>Wombat Coffee Roasters</h1>
+          <div class="slogan">We love coffee</div>
+        </div>
+</header>
+
+        <nav class="menu" id="main-menu">
+        <button class="menu-toggle" id="toggle-menu">
+          toggle menu
+        </button>
+        <div class="menu-dropdown">
+          <ul class="nav-menu">
+            <li><a href="/">About</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/brew">Brew</a></li>
+          </ul>
+        </div>
+        </nav>
+    <h4>Cappuccino</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/1ae5f0330bc6464aabbe6ba3d2c1c1db.png" width="300" height="310">
+    <br>
+    <h4>Espresso</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/5e4ec4baf5004217bce8e054231234fc.png" width="300" height="310">
+    <br>
+    <h4>Latte</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/0e411cc6fb61422e8109546f3888043e.png" width="300" height="310">
+    <br>
+    <h4>Latte</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/0e411cc6fb61422e8109546f3888043e.png" width="300" height="310">
+    <br>
+    <h4>Mocca</h4>
+    <img src="http://www.cafe-amazon.com/BackEnd/TempProducts/5cc8336ff7c443d78f0a8adc38d5cc5e.png" width="300" height="310">
+    <br>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
